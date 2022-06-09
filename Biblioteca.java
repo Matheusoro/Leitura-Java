6ec590c41976700656bc2d0d5110d41ed26d2163
Matheus <105460990+Matheusoro@users.noreply.github.com>
2022-06-09 00:24:47 -0300

    Add files via upload

---
+++ Biblioteca.java
@@ -0,0 +1,16 @@
+public class Biblioteca {
+    public static void main(String[] args) {
+        LivroFisico l1 = new LivroFisico("kafta", 2);
+
+        System.out.println(l1.verificarDisponibilidade());
+        l1.emprestarLivro();
+        l1.emprestarLivro();
+        System.out.println(l1.verificarDisponibilidade());
+
+        Ebook e1 = new Ebook("meu pé de laranja lima");
+        System.out.println(l1.verificarDisponibilidade());
+        e1.emprestarLivro();
+        e1.emprestarLivro();
+        System.out.println(l1.verificarDisponibilidade());
+    }
+}
